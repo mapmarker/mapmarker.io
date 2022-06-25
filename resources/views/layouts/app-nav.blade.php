2bcc2a110c265592362358ddd071f5993269953f
Jonas Weigert <weigert.jonas@gmail.com>
2022-06-25 16:49:40 -0700

    feat(homepage): added homepage with latest release

--- resources/views/layouts/app-nav.blade.php
+++ resources/views/layouts/app-nav.blade.php
@@ -33,17 +33,19 @@
                     <div class="hidden sm:block sm:ml-6">
                         <div class="flex space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="#" class="bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
-                                aria-current="page">Dashboard</a>
+                            <a href="/"
+                                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                aria-current="page">Home</a>
 
-                            <a href="#"
-                                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
+                            <a href="docs"
+                                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Docs</a>
 
-                            <a href="#"
-                                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
+                            <a href="/changelog"
+                                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Changelog</a>
 
-                            <a href="#"
-                                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Calendar</a>
+                            <a href="https://status.mapmarker.io" target="_blank"
+                                class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">API
+                                Status</a>
                         </div>
                     </div>
                 </div>
@@ -61,17 +63,19 @@
         <div x-show="mobileMenuOpen" x-cloak id="mobile-menu" class="sm:hidden">
             <div class="px-2 pt-2 pb-3 space-y-1">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
-                    aria-current="page">Dashboard</a>
+                <a href="/"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                    aria-current="page">Home</a>
 
-                <a href="#"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Team</a>
+                <a href="/docs"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Docs</a>
 
-                <a href="#"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Projects</a>
+                <a href="/changelog"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Changelog</a>
 
-                <a href="#"
-                    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Calendar</a>
+                <a href="https://status.mapmarker.io" target="_blank"
+                    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">API
+                    Status</a>
             </div>
         </div>
     </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,28 @@
 @extends('layouts.app-nav')
 
 @section('content')
-    <div class="container mx-auto px-2">
-        <div class="pt-16">
-            <h1 class="font-bold text-5xl mb-8">Generate complex map markers on the fly.</h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, commodi voluptate dolorum provident
-            perferendis tempore dicta adipisci magni voluptatem veniam fugiat maxime, eum ex fuga harum laborum facilis
-            magnam temporibus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed id placeat sint maiores culpa
-            inventore possimus, delectus facilis, numquam beatae ipsum et eos animi vitae ab accusantium quaerat optio
-            itaque!
+    <div class="container mx-auto px-2 text-gray-200">
+
+        <div class="md:grid grid-cols-2 gap-8">
+            <div class="col-span-2">
+                <div class="rounded-lg mb-8 md:mb-0 hero">
+                    <div class="rounded-lg lg:text-center bg-indigo-700/80 p-6 lg:py-36">
+                        <h1 class="text-3xl lg:text-5xl font-bold">
+                            Generate complex map markers on the fly.
+                        </h1>
+                        <p class="text-xl mt-6">
+                            Simplify building complex map-based experiences for modern applications.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="bg-slate-800 p-8 rounded-lg mb-8 md:mb-0">
+                    <div class="changelog changelog-slim">
+                        <x-markdown>{{ $versionEntry }}</x-markdown>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
