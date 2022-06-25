7c2abec112ab2695db764c9b629874895a9bf6d0
Jonas Weigert <weigert.jonas@gmail.com>
2022-06-25 14:23:18 -0700

    test(changlog): added test for changelog page with screenshot

---
+++ tests/Browser/ChangelogTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Browser;
+
+use Laravel\Dusk\Browser;
+use Tests\DuskTestCase;
+
+class HomeTest extends DuskTestCase
+{
+    public function test_homepage()
+    {
+        $this->browse(function (Browser $browser) {
+
+            $expectedVersionString = 'v' . \Composer\InstalledVersions::getRootPackage()['pretty_version'];
+
+            $browser->visit('/changelog')
+                ->screenshot('page.changelog')
+                ->assertSee($expectedVersionString)
+                ->assertSee('Changelog');
+        });
+    }
+}
