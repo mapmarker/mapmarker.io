1f018463906b991a2e4342121577e5837ab81a2c
Jonas Weigert <weigert.jonas@gmail.com>
2022-06-28 10:43:16 -0700

    feat(fa-v5): added v2 support for icons
    includes CreateIcon action and IconController

--- app/Http/Controllers/API/v2/FontAwesome/v5/IconController.php
+++ app/Http/Controllers/API/v2/FontAwesome/v5/IconController.php
@@ -1,20 +1,25 @@
 <?php
 
-namespace App\Http\Controllers\API\v1\FontAwesome\v5;
+namespace App\Http\Controllers\API\v2\FontAwesome\v5;
 
 use App\Actions\FontAwesome\v5\CreateIcon;
+use App\Actions\Image\ResizeToHeight;
+use App\Actions\Image\TrimTransparentBorder;
 use App\Http\Controllers\Controller as Controller;
 use Illuminate\Http\Request;
 
 class IconController extends Controller
 {
-    const SCALE = 1.0;
+    const SCALE = 2.0;
 
-    const FORCE_ODD_SIZE = false;
+    const FORCE_ODD_SIZE = true;
 
     public function show(Request $request)
     {
+        $size = $request->get('size') ?: 30;
         $img = CreateIcon::run($request->all(), self::SCALE, self::FORCE_ODD_SIZE);
+        TrimTransparentBorder::run($img);
+        ResizeToHeight::run($img, $size);
 
         return $img->response('png');
     }
