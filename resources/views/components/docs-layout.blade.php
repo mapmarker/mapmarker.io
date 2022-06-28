a0fed3a872369116ee4c7c0e0b4bcaa067f0f54c
Jonas Weigert <weigert.jonas@gmail.com>
2022-06-28 13:16:08 -0700

    feat(docs): adding navigation structure

--- resources/views/components/docs-layout.blade.php
+++ resources/views/components/docs-layout.blade.php
@@ -5,10 +5,23 @@
             <div class="md:grid grid-cols-5 gap-8">
                 {{-- DOCS NAVIGATION --}}
                 <div>
-                    <a href="/documentation" class="block">Getting Started</a>
-                    <a href="/license" class="block">License</a>
-                    <a href="/terms" class="block">Terms of Service</a>
-                    <a href="/privacy" class="block">Privacy Policy</a>
+                    <div class="mb-6">
+                        <a href="/documentation" class="block">Getting Started</a>
+                    </div>
+
+                    <div class="mb-6">
+                        <div class="text-black text-sm text-slate-300 mb-2">Integrations</div>
+                        <a href="/documentation/integrations#bing-maps" class="block">Bing Maps</a>
+                        <a href="/documentation/integrations#google-maps" class="block mb-1">Google Maps</a>
+                        <a href="/documentation/integrations#mapbox" class="block">Mapbox</a>
+                    </div>
+
+                    <div class="mb-6">
+                        <div class="text-black text-sm text-slate-300 mb-2">Legal</div>
+                        <a href="/license" class="block mb-1">License</a>
+                        <a href="/terms" class="block mb-1">Terms of Service</a>
+                        <a href="/privacy" class="block">Privacy Policy</a>
+                    </div>
                 </div>
 
                 {{-- DOCS CONTENT --}}
