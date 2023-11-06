a436340873d52b61d7ab1ae617ca3ff49670bc40
Jonas Weigert <weigert.jonas@gmail.com>
2023-11-05 17:32:56 -0800

    feat(docs): updated homepage / docs components to use new svg icons

--- resources/views/docs/font-awesome/v6/index.blade.php
+++ resources/views/docs/font-awesome/v6/index.blade.php
@@ -6,12 +6,7 @@
     <div class="md:grid grid-cols-3 gap-8">
         <div class="col-span-3">
             <x-docs-box>
-                <div class="mb-4">
-                    <img src="/api/v3/font-awesome/v6/icon?icon=fa-brands+fa-fort-awesome&size=50&color=6013CC" class="m-0 inline h-12 -mr-12 w-[49px]" />
-                    <img src="/api/v3/font-awesome/v6/icon?icon=fa-brands+fa-fort-awesome&&size=50&color=992DE5" class="m-0 inline h-12 -mr-12 w-[49px]" />
-                    <img src="/api/v3/font-awesome/v6/icon?icon=fa-brands+fa-fort-awesome&&size=50&color=BC5AF4" class="m-0 inline h-12 -mr-12 w-[49px]" />
-                    <img src="/api/v3/font-awesome/v6/icon?icon=fa-brands+fa-fort-awesome&&size=50&color=CE86F5" class="m-0 inline h-12 -mr-12 w-[49px]" />
-                </div>
+                <x-icon-spectrum icon="fa-brands fa-fort-awesome" iconClass="-mr-12 w-[49px]" />
 
 
                 <h2>Font Awesome 6</h2>
