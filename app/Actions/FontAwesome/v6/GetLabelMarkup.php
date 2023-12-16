ab9c6f98487fbb517c018a0aade94039ce00b8aa
Jonas Weigert <weigert.jonas@gmail.com>
2023-12-16 10:04:30 -0800

    feat(label animations): added POC label "blink" animation for map markers

--- app/Actions/FontAwesome/v6/GetLabelMarkup.php
+++ app/Actions/FontAwesome/v6/GetLabelMarkup.php
@@ -17,13 +17,69 @@ class GetLabelMarkup
         $labelColor = $request->get('lc', '#D9534F');
         $labelFont = $request->get('lf', 'Arial');
         $labelFontColor = $request->get('lfc', '#FFF');
+
+        // LABEL ANIMATION
+        $labelAnimation = $request->get('labelAnimation', null);
+        $labelAnimationDuration = $request->get('labelAnimationDuration', '1s');
+        $labelAnimationMarkup = null;
+
+        if ($labelAnimation == 'blink') {
+            $labelAnimationMarkup = <<<EOD
+                <style>
+
+                    @keyframes blink {
+                        0% {
+                            opacity: 1;
+                        }
+                        49% {
+                            opacity: 1;
+                        }
+                        50% {
+                            opacity: 0;
+                        }
+                        99% {
+                            opacity: 0;
+                        }
+                        100% {
+                            opacity: 1;
+                        }
+                    }
+
+                    @-webkit-keyframes blink {
+                        0% {
+                            opacity: 1;
+                        }
+                        49% {
+                            opacity: 1;
+                        }
+                        50% {
+                            opacity: 0;
+                        }
+                        99% {
+                            opacity: 0;
+                        }
+                        100% {
+                            opacity: 1;
+                        }
+                     }
+
+                    .labelAnimation {
+                        animation: blink {$labelAnimationDuration} linear infinite;
+                        -webkit-animation: blink {$labelAnimationDuration} linear infinite;
+                    }
+                    </style>
+            EOD;
+        }
+
+        // GENERATE LABEL MARKUP
         $labelMarkup = '';
         if ($labelText) {
             $labelMarkup = <<<EOD
             <!--! Icon Label -->
-            <svg x="60%" y="60%" width="40%" height="40%" viewbox="0 0 100 100">
+            <svg x="60%" y="60%" width="40%" height="40%" viewbox="0 0 100 100" class="labelAnimation">
                 <circle cx="50%" cy="50%" fill="{$labelColor}" r="50%" />
                 <text x="50%" y="50%" fill="{$labelFontColor}" text-anchor="middle" dy=".3em" font-size="{$labelTextSize}" font-family="{$labelFont}">{$labelText}</text>
+                {$labelAnimationMarkup}
             </svg>
             EOD;
         }
